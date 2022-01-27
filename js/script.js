0df80aa5f333366a674575a93fae95b477546eb9
volvojakobsen <91479420+volvojakobsen@users.noreply.github.com>
2022-01-27 19:02:05 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,3 +37,42 @@ heading.style.fontSize = "2rem";
 
 heading.className = "subheading";
 
+
+//question 5
+
+const paragraphs = document.querySelectorAll("p");
+
+for(var i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = "red";
+}
+
+//question 6
+
+const resultContainer = document.querySelector(".results");
+
+resultContainer.innerHTML = " <p>New paragraph</p>";
+resultContainer.style.backgroundColor = "yellow";
+
+//question 7
+
+function listLoop (list) {
+    for(var i = 0; i < list.length; i++) {
+        console.log(list[i].name)
+    } 
+}
+
+listLoop(cats)
+
+//question 8
+var varCat = document.querySelectorAll(".cat-container");
+varCat.innerHTML = "jajajaja"
+
+function createCats(cats) {
+    for(var i = 0; i < cats.length; i++) {
+        varCat.innerHTML = `<div><h5>${cats[i].name}</h5></div>`;
+    }
+}
+
+createCats(cats)
+
+//newHTML.innerHTML = "<div><h5>cats[i].name</h5></div>";
