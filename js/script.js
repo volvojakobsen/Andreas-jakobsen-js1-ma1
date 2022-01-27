568c827a469b26e1327f153573bb410835d2cb19
volvojakobsen <91479420+volvojakobsen@users.noreply.github.com>
2022-01-27 03:47:06 +0100

    first 4

--- js/script.js
+++ js/script.js
@@ -12,3 +12,28 @@ const cats = [
     }
 ];
 
+//question 1
+
+const cat = {
+    complain: function(){
+        console.log("meow!")
+    }
+}
+
+//console.log(cat.complain())
+
+
+//question 2
+
+var heading = document.querySelector("h3");
+
+heading.innerHTML = "updated heading";
+
+//question 3
+
+heading.style.fontSize = "2rem";
+
+//question 4
+
+heading.className = "subheading";
+
